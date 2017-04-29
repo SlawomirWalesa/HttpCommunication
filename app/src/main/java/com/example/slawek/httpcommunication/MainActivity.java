56a8c28aca582d1cec581996e91211e5ba2e742d
Slawomir Walesa <slawomir.walesa@gmail.com>
2017-04-29 14:23:36 +0200

    Add TaskDTO class
    * Added constructor and getters & setters
    * Added AssynTask

--- app/src/main/java/com/example/slawek/httpcommunication/MainActivity.java
+++ app/src/main/java/com/example/slawek/httpcommunication/MainActivity.java
@@ -7,6 +7,14 @@ import android.os.Bundle;
 import com.example.slawek.httpcommunication.dto.TaskDTO;
 import com.example.slawek.httpcommunication.model.Task;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -23,6 +31,43 @@ public class MainActivity extends AppCompatActivity {
     private class MyAsyncTask extends AsyncTask<String, Void, Task> {
         @Override
         protected Task doInBackground(String... params) {
+
+            StringBuilder builder = new StringBuilder();
+
+            String line;
+
+            String link = "https://shrouded-fjord-81597.herokuapp.com/api/health-check" + params[0].toUpperCase() + "," + params[1].toUpperCase() + "," + params[2].toUpperCase();
+            URL url = null;
+
+            try {
+                url = new URL(link);
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+            HttpURLConnection connection = null;
+            try {
+                connection = (HttpURLConnection) url.openConnection();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                InputStream in = new BufferedInputStream(connection.getInputStream());
+
+                BufferedReader reader = new BufferedReader(
+                        new InputStreamReader(in, "UTF-8"));
+                while ((line = reader.readLine()) != null) {
+                    builder.append(line);
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                connection.disconnect();
+            }
+            return takeDataFromJason(builder.toString());
+        }
+
+
             return null;
         }
 
